ff8625f1b945c0bf22dd86c7e529175191c28ee2
Dylan Sung <dys907@gmail.com>
2020-03-31 02:26:07 -0700

    cards as search results

--- scripts/results.js
+++ scripts/results.js
@@ -2,7 +2,7 @@ let queryString = decodeURIComponent(window.location.search);
 let queries = queryString.split("?");
 let search = queries[1].toLowerCase();
 console.log(search);
-
+document.getElementById("searchTitle").innerHTML = "Searching for: " + search;
 const recipes = db.collection("recipe");
 let recipeArray;
 let counter = 1;
@@ -30,142 +30,123 @@ async function getAllRecipesWithIng() {
 }
 //generate cards
 getAllRecipesWithIng().then(result => {
-    result.forEach(docSnapshot => {
-        //createCard();
+        displayCards(result);
         console.log(docSnapshot.data());
-        displayCards(docSnapshot.data());
-        //displayRecipe(docSnapshot.data(), docSnapshot.id);
+        displayRecipe(docSnapshot.data(), docSnapshot.id);
 
-    });
+
 });
 
-// function displayRecipe(recipe, id) {
-//     searchField = document.getElementById("searchContainer");
-//     var n = recipe.title;
-//     console.log(n);
-//     var para = document.createElement("div");
-//     para.setAttribute("id", id);
-//     searchField.appendChild(para);
-//     var node = document.createTextNode(n);
-//     para.appendChild(node);
-//     $("#" + id).click(function () {
-//         window.location.href = "recipe.html?" + id;
+function displayRecipe(recipe, id) {
+    searchField = document.getElementById("searchContainer");
+    var n = recipe.title;
+    console.log(n);
+    var para = document.createElement("div");
+    para.setAttribute("id", id);
+    searchField.appendChild(para);
+    var node = document.createTextNode(n);
+    para.appendChild(node);
+    $("#" + id).click(function () {
+        window.location.href = "recipe.html?" + id;
 
-//     })
-// }
+    })
+}
 //creates a card for each search result
-// function createCard() {
-//     searchContainer = document.getElementById("searchContainer");
-//     card = document.createElement("div");
-//     card.className += ("col-md-3 card");
-//     searchContainer.appendChild(card);
-//     cardImg = document.createElement("img");
-//     cardImg.className += ("card-img-top");
-//     cardImg.src = "images/ChickenBroccoli.jpg"
-//     card.appendChild(cardImg);
-//     cardBody = document.createElement("div");
-//     cardBody.className += ("card-body");
-//     card.appendChild(cardBody);
-//     recipeTitle = document.createElement("h4");
-//     recipeTitle.className += ("card-title");
-//     recipeTitle.id = "title" + counter;
-//     cardBody.appendChild(recipleTitle);
-//     recDesc = document.getElementById("p");
-//     recDesc.className += ("card-text");
-//     recDesc.id = "desc" + counter;
-//     cardBody.appendChild(recDesc);
-//     submit = document.getElementById("a");
-//     submit.className += ("btn btn-outline-secondary");
-//     submit.innerHTML = "View Recipe";
-//     submit.href = "recipe.html";
-//     cardBody.appendChild(submit);
-// }
-      // let's say we have 4 columns
+function createCard() {
+    searchContainer = document.getElementById("searchContainer");
+    card = document.createElement("div");
+    card.className += ("col-md-3 card");
+    searchContainer.appendChild(card);
+    cardImg = document.createElement("img");
+    cardImg.className += ("card-img-top");
+    cardImg.src = "images/ChickenBroccoli.jpg"
+    card.appendChild(cardImg);
+    cardBody = document.createElement("div");
+    cardBody.className += ("card-body");
+    card.appendChild(cardBody);
+    recipeTitle = document.createElement("h4");
+    recipeTitle.className += ("card-title");
+    recipeTitle.id = "title" + counter;
+    cardBody.appendChild(recipleTitle);
+    recDesc = document.getElementById("p");
+    recDesc.className += ("card-text");
+    recDesc.id = "desc" + counter;
+    cardBody.appendChild(recDesc);
+    submit = document.getElementById("a");
+    submit.className += ("btn btn-outline-secondary");
+    submit.innerHTML = "View Recipe";
+    submit.href = "recipe.html";
+    cardBody.appendChild(submit);
+}
 
-      function displayCards(CardObjects) {
+// let's say we have 4 columns
 
-        CardObjects.forEach(function (doc) {
+function displayCards(CardObjects, numCards) {
+    CardObjects.forEach(function (doc) {
+            createOneCard(doc);
 
-                createOneCard(doc);
+        })
 
-            })
+}
 
-    }
+function createOneCard(c) { //pass in one document object
 
-    function createOneCard(c) { //pass in one document object
-
-        var coldiv = document.createElement("div");
-
-        coldiv.setAttribute("class", "col-md-3");
+    var coldiv = document.createElement("div");
+    coldiv.setAttribute("class", "col-md-3");
 
 
 
-        var carddiv = document.createElement("div");
-
-        carddiv.setAttribute("class", "card");
-
-
-
-        var img = document.createElement("img");
-
-        img.setAttribute("class", "card-img-top");
-
-        img.setAttribute("src", "food.jpg");
+    var carddiv = document.createElement("div");
+    carddiv.setAttribute("class", "card");
+    var img = document.createElement("img");
+    img.setAttribute("class", "card-img-top");
+    img.setAttribute("src", "food.jpg");
 
 
 
-        var cardbodydiv = document.createElement("div");
-
-        cardbodydiv.setAttribute("class", "card-body");
-
-
-
-        var h4 = document.createElement("h4");
-
-        h4.setAttribute("class", "card-title");
-
-        var text = document.createTextNode(c.data().name);
-
-        h4.appendChild(text);
+    var cardbodydiv = document.createElement("div");
+    cardbodydiv.setAttribute("class", "card-body");
 
 
 
-        var p = document.createElement("p");
-
-        p.setAttribute("class", "card-text");
-
-        var text = document.createTextNode(c.data().description);
-
-        p.appendChild(text);
+    var h4 = document.createElement("h4");
+    h4.setAttribute("class", "card-title");
+    var text = document.createTextNode(c.data().title);
+    h4.appendChild(text);
 
 
 
-        var a = document.createElement("a");
+    var p = document.createElement("p");
 
-        a.setAttribute("href", c.data().id + ".html");
-
-        a.setAttribute("class", "btn btn-outline-secondary");
-
-        var text = document.createTextNode("View Recipe");
-
-        a.appendChild(text);
+    p.setAttribute("class", "card-text");
+    var text = document.createTextNode(c.data().description);
+    p.appendChild(text);
 
 
 
-        cardbodydiv.appendChild(h4);
+    var a = document.createElement("a");
+    a.setAttribute("href","recipe.html?" + c.id);
+    a.setAttribute("class", "btn btn-outline-secondary");
 
-        cardbodydiv.appendChild(p);
+    var text = document.createTextNode("View Recipe");
 
-        cardbodydiv.appendChild(a);
+    a.appendChild(text);
 
-        img.appendChild(cardbodydiv);
 
-        carddiv.appendChild(img);
 
-        carddiv.appendChild(cardbodydiv);
+    cardbodydiv.appendChild(h4);
 
-        coldiv.appendChild(carddiv);
+    cardbodydiv.appendChild(p);
 
-        document.getElementById("cards").appendChild(coldiv);
+    cardbodydiv.appendChild(a);
 
-    }
+    img.appendChild(cardbodydiv);
+
+    carddiv.appendChild(img);
+
+    carddiv.appendChild(cardbodydiv);
+
+    coldiv.appendChild(carddiv);
+
+    document.getElementById("cards").appendChild(coldiv);
+}
